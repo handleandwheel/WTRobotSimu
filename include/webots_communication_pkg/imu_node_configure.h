b85d3aa2e5072c8fb56a68b0fad0d82f8fe2b882
handleandwheel <liuyumingyouxiang@126.com>
2021-09-23 11:27:45 +0800

    nothing changed

---
+++ include/webots_communication_pkg/imu_node_configure.h
@@ -0,0 +1,26 @@
+// imu_node_configure.h
+// Environment: ubuntu 20.04, ros noetic
+// Created by handleandwheel on 2021/9/21.
+//
+
+#ifndef WEBOTS_WS_IMU_NODE_CONFIGURE_H
+#define WEBOTS_WS_IMU_NODE_CONFIGURE_H
+
+#include "ros/ros.h"
+
+using namespace std;
+
+class IMUNodeConfig
+{
+public:
+    IMUNodeConfig(){}
+    void set(const ros::NodeHandle &);
+    int sampling_time_period;
+};
+
+void IMUNodeConfig::set(const ros::NodeHandle &nh)
+{
+    nh.param("/imu_node/sampling_time_period", sampling_time_period, 64);
+}
+
+#endif //WEBOTS_WS_IMU_NODE_CONFIGURE_H
